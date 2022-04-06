4932b25fc1e7b4bf0e1002a3383f61b35a019e3e
Jerald Tonmoy Dias <59910969+Jerald-tonmoy-dias@users.noreply.github.com>
2022-04-06 15:18:27 +0600

    magnify added

--- assets/js/main.js
+++ assets/js/main.js
@@ -186,38 +186,38 @@
         // });
 
         // vdo popup activation
-        // $('.popup-link').magnificPopup({
-        //     type: 'iframe',
-        //     // other options
-        //     iframe: {
-        //         markup: '<div class="mfp-iframe-scaler">' +
-        //             '<div class="mfp-close"></div>' +
-        //             '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
-        //             '</div>',
+        $('.popup-link').magnificPopup({
+            type: 'iframe',
+            // other options
+            iframe: {
+                markup: '<div class="mfp-iframe-scaler">' +
+                    '<div class="mfp-close"></div>' +
+                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                    '</div>',
 
-        //         patterns: {
-        //             youtube: {
-        //                 index: 'youtube.com/',
+                patterns: {
+                    youtube: {
+                        index: 'youtube.com/',
 
-        //                 id: 'v=',
+                        id: 'v=',
 
-        //                 src: 'https://www.youtube.com/embed/%id%?autoplay=1'
-        //             },
-        //             vimeo: {
-        //                 index: 'vimeo.com/',
-        //                 id: '/',
-        //                 src: '//player.vimeo.com/video/%id%?autoplay=1'
-        //             },
-        //             gmaps: {
-        //                 index: '//maps.google.',
-        //                 src: '%id%&output=embed'
-        //             }
+                        src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+                    },
+                    vimeo: {
+                        index: 'vimeo.com/',
+                        id: '/',
+                        src: '//player.vimeo.com/video/%id%?autoplay=1'
+                    },
+                    gmaps: {
+                        index: '//maps.google.',
+                        src: '%id%&output=embed'
+                    }
 
-        //         },
+                },
 
-        //         srcAction: 'iframe_src',
-        //     }
-        // });
+                srcAction: 'iframe_src',
+            }
+        });
 
         // food-price range-slider
         // $(function () {
